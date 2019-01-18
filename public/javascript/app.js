50f62a270a291d9fb5ecb790199078571a39f6c3
MAwais <mawais54013@outlook.com>
2019-01-17 21:33:15 -0800

    updated changes/fixed issues

---
+++ public/javascript/app.js
@@ -0,0 +1,26 @@
+$(document).ready( function() {
+
+    // SMOOTH SCROLLING
+    $('a[href*="#"]').click(function (event) {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 62
+                }, 1000);
+            }
+        }
+    });
+    
+    // CLOSE NAVBAR TOGGLER WHEN ANCHOR CLICKED
+    $(function() {
+        $('.navbar a').on('click', function(){ 
+            if($('.navbar-toggler').css('display') !='none'){
+                $('.navbar-toggler').trigger( "click" );
+            }
+        });
+    });
+    
+    });
